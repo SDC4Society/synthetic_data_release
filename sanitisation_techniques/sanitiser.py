8d7422a347cf6f7ccb8f504c73b3bf56e7f1d9bb
Kohei Tanaka <you@example.com>
2026-04-09 15:59:57 +0900

    feat: add get_output_metadata hook to Sanitiser base class

--- sanitisation_techniques/sanitiser.py
+++ sanitisation_techniques/sanitiser.py
@@ -6,3 +6,11 @@ class Sanitiser(object):
     def sanitise(self, data):
         """ Apply a privacy policy to the data. """
         return NotImplementedError('Method needs to be overwritten by a subclass')
+
+    def get_output_metadata(self, input_metadata):
+        """Return metadata describing the sanitiser's output schema.
+
+        Default: identity (output schema matches input).
+        Override in subclasses that transform the schema.
+        """
+        return input_metadata
